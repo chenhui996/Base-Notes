cb34afb0068b4e19b63b1b8ab4982e4853e0e860
cain <chen562450669@qq.com>
2020-08-19 15:00:17 +0800

    notes update 20200819

--- XML/app2/app.js
+++ XML/app2/app.js
@@ -1,24 +1,36 @@
-const Koa = require('koa');
-const KoaStaticCache = require('koa-static-cache');
-const KoaRouter = require('koa-router');
+const Koa = require("koa");
+const KoaStaticCache = require("koa-static-cache");
+const KoaRouter = require("koa-router");
 
 const app = new Koa();
 const router = new KoaRouter();
 
-app.use(KoaStaticCache('static',{
-    prefix: '/static',
+app.use(
+  KoaStaticCache("static", {
+    prefix: "/static",
     gzip: true,
-    dynamic: true
-}));
+    dynamic: true,
+  })
+);
 
-router.get('/check', async ctx=>{
-    ctx.body = "检查登录";
+//user
+const users = ["cain", "snake"];
+let user = null;
+
+router.get("/check", async (ctx) => {
+    if(!user){
+        ctx.body = { code: -1, message: "还没登录" };
+    }
+    else{
+        ctx.body = { code: 0, message: "登陆成功" };
+    }
 });
-router.get('/login', async ctx=>{
-    ctx.body = "登录成功";
+router.get("/login", async (ctx) => {
+  user = {};
+  ctx.body = "登录成功";
 });
 
 app.use(router.routes());
-app.listen(8081,()=>{
-    console.log("Server running at http://localhost:8081");
-})
+app.listen(8081, () => {
+  console.log("Server running at http://localhost:8081");
+});
